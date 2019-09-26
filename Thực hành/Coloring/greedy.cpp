3c2424b159cee121f3a74062184b149d1ce68fc4
confusedvariable <phuoct52@gmail.com>
2019-09-26 09:17:51 +0700

    Thêm sắp xếp thứ tự

--- Thực hành/Coloring/greedy.cpp
+++ Thực hành/Coloring/greedy.cpp
@@ -44,15 +44,56 @@ public:
         return find(isColoredWith.begin() + 1, isColoredWith.end(), -1) == isColoredWith.end();
     }
 
-    vector<vector<int>> coloring()
+    // Đếm bậc của từng đỉnh
+    vector<vector<int>> countRank()
+    {
+        vector<vector<int>> result;
+        vector<int> pair;
+
+        for (int i = 0; i < nVertex; i++)
+        {
+            pair.push_back(i + 1);
+            pair.push_back(0);
+
+            result.push_back(pair);
+            pair.clear();
+        }
+
+        for (int i = 0; i < nVertex; i++)
+        {
+            for (int j = 0; j < nVertex; j++)
+            {
+                if (matrix[i][j] == 1)
+                {
+                    result[i][1]++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    // Sắp xếp đỉnh theo bậc giảm dần
+    static bool sortRankDes(const vector<int> &v1, const vector<int> &v2)
+    {
+        return v1[1] > v2[1];
+    }
+
+    vector<vector<int>> greedyColoring(bool isOrdered = false)
     {
         vector<vector<int>> result;
         vector<int> vertexList;
+        // Đếm bậc
+        vector<vector<int>> rank = countRank();
         // Vector lưu màu của từng đỉnh
         // -1 tức chưa tô
         vector<int> isColoredWith(this->nVertex + 1, -1);
         // Màu đang sử dụng
         int currColor = 0;
+
+        // Sắp xếp
+        if (isOrdered == true)
+            sort(rank.begin(), rank.end(), sortRankDes);
 
         /**
         Khi chưa tô màu hết tất cả các đỉnh
@@ -62,10 +103,12 @@ public:
         while (!isDoneColoring(isColoredWith))
         {
             // Duyệt từng đỉnh một và thực hiện tô màu các đỉnh hợp lệ
-            for (int i = 1; i <= this->nVertex; i++)
+            for (int i = 0; i < this->nVertex; i++)
             {
+                // Đỉnh đang xét
+                int currVert = rank[i][0];
                 // Nếu đỉnh đó chưa được tô màu
-                if (isColoredWith[i] == -1)
+                if (isColoredWith[currVert] == -1)
                 {
                     // Màu đang sử dụng có thích hợp tô cho đỉnh i không
                     bool isValid = true;
@@ -74,7 +117,7 @@ public:
                     // Nếu trùng màu thì màu đang sử dụng không thích hợp cho đỉnh i
                     for (int j = 1; j <= this->nVertex; j++)
                     {
-                        if (isConnected(i, j) && isColoredWith[j] == currColor)
+                        if (isConnected(currVert, j) && isColoredWith[j] == currColor)
                         {
                             isValid = false;
                             break;
@@ -83,8 +126,8 @@ public:
                     // Nếu màu thích hợp cho đỉnh i thì tô màu cho nó
                     if (isValid == true)
                     {
-                        vertexList.push_back(i);
-                        isColoredWith[i] = currColor;
+                        vertexList.push_back(currVert);
+                        isColoredWith[currVert] = currColor;
                     }
                 }
             }
@@ -113,8 +156,20 @@ int main()
         g.addEdge(a, b);
     }
 
-    vector<vector<int>> result = g.coloring();
-    cout << "\n-------Coloring result-------\n";
+    vector<vector<int>> result = g.greedyColoring();
+    cout << "\n-------Greedy coloring result-------\n";
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << "Color " << i << ": ";
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    result = g.greedyColoring(true);
+    cout << "\n-------Order coloring result-------\n";
     for (int i = 0; i < result.size(); i++)
     {
         cout << "Color " << i << ": ";
